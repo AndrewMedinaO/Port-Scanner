267c133134e084bc2860c4344dc2f1dd19f27246
Andrew Medina <andrewmedina12@outlook.com>
2026-06-02 17:35:44 -0400

    Added Banner Fetching and more timers and prints

--- SocketGrabber.py
+++ SocketGrabber.py
@@ -11,21 +11,55 @@ def ScannerFunc(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.1)
     result = s.connect_ex((ip, port))
-    
+    s.close()
     if result == 0:
         return port
     
-    
-    s.close()
+
+def Grab_Banner(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        s.connect((ip,port))
+        s.send(b"hi")
+        banner = s.recv(1024)
+        banner.decode("utf-8", errors="ignore").strip()
+        s.close()
+
+        return banner
+    except: 
+        return None
+
+
+
+
+Open_Ports = []
 start = time.time()
+print(f"Scanning for Ports")
 with concurrent.futures.ThreadPoolExecutor() as executor:
 
-    results = executor.map(ScannerFunc, range(1,1025))
+    results = executor.map(ScannerFunc, range(1,600))
     for result in results:
-        if result != None:
+        if result is not None:
             print(f"{ip}:{result} is open")
-
+            Open_Ports.append(result)
 
 end = time.time()
 
 print(f"Scan finished in {end - start:.2f} seconds")
+
+
+print(f"-- Begining Banner fetch--")
+
+startnew = time.time()
+for port in Open_Ports:
+    banner = Grab_Banner(ip, port)
+    if banner:
+        print(f"{ip}:{port} is open: {banner}")
+    else:
+        print(f"{ip}:{port} is open: (no banner sad  ))")
+
+
+endnew = time.time()
+
+print(f"Banner Fetch finished in {endnew - startnew:.2f} seconds")

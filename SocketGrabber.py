08271f652756e97b66158e8b28d4fbce762c6f64
Andrew Medina <andrewmedina12@outlook.com>
2026-06-02 17:07:26 -0400

    Now loops through ports 1-1024 on the local IP

--- SocketGrabber.py
+++ SocketGrabber.py
@@ -1,11 +1,19 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(2)
+
+
 ip = "127.0.0.1"
-port = 8000
-result = s.connect_ex((ip, port))
-if result == 0:
-    print(f"Port {port} open on {ip}")
-else:
-    print(f"Port {port} closed on {ip} (code {result})")
-s.close()
+
+
+
+
+
+for port in range(1,9000):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.001)
+    result = s.connect_ex((ip, port))
+    
+    if result == 0:
+        print(f"Port {port} open on {ip}")
+    
+    
+    s.close()

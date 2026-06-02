bb94a8d46f5917fae2d5a3310ac6ba71e6108e72
Andrew Medina <andrewmedina12@outlook.com>
2026-06-02 17:20:50 -0400

    Added ConcurrentFutures Module to shorten search, added timer to see time elapsed

--- SocketGrabber.py
+++ SocketGrabber.py
@@ -1,19 +1,31 @@
 import socket
-
-
+import concurrent.futures
+import time
 ip = "127.0.0.1"
 
 
 
 
 
-for port in range(1,9000):
+def ScannerFunc(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.001)
+    s.settimeout(0.1)
     result = s.connect_ex((ip, port))
     
     if result == 0:
-        print(f"Port {port} open on {ip}")
+        return port
     
     
     s.close()
+start = time.time()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+
+    results = executor.map(ScannerFunc, range(1,1025))
+    for result in results:
+        if result != None:
+            print(f"{ip}:{result} is open")
+
+
+end = time.time()
+
+print(f"Scan finished in {end - start:.2f} seconds")
